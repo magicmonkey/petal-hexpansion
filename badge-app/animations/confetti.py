cdbe1bdfb91ce3ea23071add226a2a424361471e
Lorna Jane Mitchell <lorna@lornajane.net>
2026-06-27 14:22:38 +0100

    Add decay function and remove loop limit

--- badge-app/animations/confetti.py
+++ badge-app/animations/confetti.py
@@ -19,18 +19,33 @@ band_offset = {
     2: OUTER_BAND_COUNT + MIDDLE_BAND_COUNT,
 }
 
+active_leds = {}
+
 async def anim(petals):
+    # clean sheet to start
     petals.all_led_off()
-    for i in range(50):
+
+    # main loop
+    while(True):
         await asyncio.sleep_ms(100)
         led_random(petals)
+        decay(petals)
 
 def led_random(petals):
     # pick a band (outer, middle, inner)
     band = random.randint(0,2)
     # pick an LED
     led = random.randint(0, band_count[band] - 1) + band_offset[band]
-    print("LED: " + str(led))
-    petals.one_led_on(led)
 
+    # turn on the LED and add to the list
+    petals.one_led_on(led)
+    active_leds[led] = 5 + random.randint(0, 10) # TTL
+
+def decay(petals):
+    # reduce TTL, turn off and delist if zero
+    for led in list(active_leds):
+        active_leds[led] -= 1
+        if active_leds[led] < 1:
+            del active_leds[led]
+            petals.one_led_off(led)
 
