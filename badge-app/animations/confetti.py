70f85260dc1e2570cf65ec07c4304aa42179b085
Lorna Jane Mitchell <lorna@lornajane.net>
2026-06-27 14:02:07 +0100

    Make each band equally likely

--- badge-app/animations/confetti.py
+++ badge-app/animations/confetti.py
@@ -1,15 +1,33 @@
 import time
 import random
 
+INNER_BAND_COUNT = 5
+MIDDLE_BAND_COUNT = 12
+OUTER_BAND_COUNT = 19
+
+band_count = {
+    0: OUTER_BAND_COUNT,
+    1: MIDDLE_BAND_COUNT,
+    2: INNER_BAND_COUNT,
+}
+
+band_offset = {
+    0: 0,
+    1: OUTER_BAND_COUNT,
+    2: OUTER_BAND_COUNT + MIDDLE_BAND_COUNT,
+}
+
 def anim(petals):
+    petals.all_led_off()
     for i in range(50):
         time.sleep_ms(100)
         led_random(petals)
 
-
 def led_random(petals):
+    # pick a band (outer, middle, inner)
+    band = random.randint(0,2)
     # pick an LED
-    led = random.randint(0, 35)
+    led = random.randint(0, band_count[band] - 1) + band_offset[band]
     print("LED: " + str(led))
     petals.one_led_on(led)
 
